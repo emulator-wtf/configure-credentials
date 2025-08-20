0164707077449efef59341b972cf2ef87663e3b8
Madis Pink <1147723+madisp@users.noreply.github.com>
2025-08-20 15:46:51 +0300

    feat: make requests to oidc auth endpoint (#2)
    * feat: make requests to oidc auth endpoint
    
    * chore: install dist node_modules
    
    * chore: update dist
    
    * tests: run ew-cli to test obtained token
    
    * fix: use correct input name when receiving oidc-configuration-id
    
    * feat: improve missing permission output
    
    * chore: update dist
    
    * fix: give id-token permission to test workflow
    
    * fix(security): mask temp api token as a secret
    
    * chore: update dist

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log('todo');
+import {getInput, getIDToken, exportVariable, setSecret} from '@actions/core'
+import { authenticateOidc } from './api'
+
+async function run() {
+  const oidcConfigurationUuid = getInput('oidc-configuration-id', { required: true })
+
+  try {
+    const oidcToken = await getIDToken('api://emulator.wtf')
+
+    // make api request to the OIDC configuration endpoint
+    const response = await authenticateOidc({ oidcConfigurationUuid, oidcToken })
+
+    // export EW_API_TOKEN to environment
+    exportVariable('EW_API_TOKEN', response.apiToken)
+    // mark it as a secret
+    setSecret(response.apiToken)
+  } catch (error) {
+    // check for "Unable to get ACTIONS_ID_TOKEN_REQUEST_URL env variable" error
+    if (error instanceof Error && error.message.includes('Unable to get ACTIONS_ID_TOKEN_REQUEST_URL env variable')) {
+      throw new Error("This action requires the 'id-token' permission to be set in the workflow. Please add the following to your workflow file:\n\n" +
+        "permissions:\n  id-token: write\n")
+    } else {
+      throw error
+    }
+  }
+}
+
+// noinspection JSIgnoredPromiseFromCall
+run()
